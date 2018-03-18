7fd79a387f68282b8142ec5b442f1e564df6523b
neil605164 <neil605164work@gmail.com>
2018-03-18 22:12:10 +0800

    [修改] 上傳檔案api

--- controller/file/upload_file.js
+++ controller/file/upload_file.js
@@ -4,17 +4,14 @@ var moment = require('moment');
 var formidable = require('formidable');
 
 function upload_file(req, res) {
-    let dirName = path.dirname(file);
-    let extName = path.extname(file);
-    let fileName = path.basename(file, extName);
-
 	var form = new formidable.IncomingForm();
     form.uploadDir = path.join('/home/hsieh/Node_Blog/', 'tmp');   //文件保存的临时目录为当前项目下的tmp文件夹
     form.maxFieldsSize = 100 * 1024 * 1024;  //用户头像大小限制为最大100M  
     form.keepExtensions = true;        //使用文件的原扩展名
 
     form.parse(req, function (err, fields, file) {
-		var filePath = '';
+        var filePath = '';
+        console.log(fields)
 
         //如果提交文件的form中将上传文件的input名设置为tmpFile，就从tmpFile中取上传文件。否则取for in循环第一个上传的文件。
         if(file.tmpFile){
@@ -45,7 +42,6 @@ function upload_file(req, res) {
             //移动文件
             fs.rename(filePath, targetFile, function (err) {
                 if (err) {
-                    console.info(err);
                     res.json({code:-1, message:'操作失败'});
                 } else {
                     //上传成功，返回文件的相对路径
